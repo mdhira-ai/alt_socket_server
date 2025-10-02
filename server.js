9af7806b2de702d63e4e9dbac15073a2a7bec49c
MD HABIBOR RAHMAN HIRA <68804057+mdhira-ai@users.noreply.github.com>
2025-10-01 21:54:19 -0400

    fix: change default server port from 3001 to 3000

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ io.on("connection", async (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
