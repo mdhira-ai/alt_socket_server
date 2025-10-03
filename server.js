2783abae0f5b02ccaf6957eac005d46993bb171b
MD HABIBOR RAHMAN HIRA <68804057+mdhira-ai@users.noreply.github.com>
2025-10-02 23:27:24 -0400

    fix: update main entry point and server port configuration

--- server.js
+++ server.js
@@ -171,7 +171,7 @@ io.on("connection", async (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
